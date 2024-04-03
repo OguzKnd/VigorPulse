847a309371ee1e9c4bc039dbb79f1be9e12f0ec0
Oguz Kanda <exporler95@gmail.com>
2024-04-03 14:39:57 +0300

    Initial Commit

---
+++ VigorPulse/VigorPulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  VigorPulseApp.swift
+//  VigorPulse
+//
+//  Created by Oğuz Kanda on 3.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct VigorPulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
